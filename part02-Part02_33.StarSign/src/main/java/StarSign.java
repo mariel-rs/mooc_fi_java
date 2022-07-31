e5cceb96896234a08c54157f49a2852539030846
mariel-rs <mariel.rsal@gmail.com>
2022-07-31 15:35:35 -0500

    Progress section 3 of part2

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -2,35 +2,60 @@
 public class StarSign {
 
     public static void main(String[] args) {
-
         
         //The tests are not checking the main, so you can modify it freely.
         //NB: If the tests don't seem to pass, you should try the methods here
         //in the main to make sure they print the correct shapes!
         
-        printStars(3);
+        printStars(1);
         System.out.println("\n---");  // printing --- between the shapes
-        printSquare(4);
-        System.out.println("\n---");
-        printRectangle(5, 6);
-        System.out.println("\n---");
-        printTriangle(3);
-        System.out.println("\n---");
+        // printSquare(3);
+        // System.out.println("\n---");
+        // printRectangle(5, 6);
+        // System.out.println("\n---");
+        // printTriangle(3);
+        // System.out.println("\n---");
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+
+        String stars = "";
+
+        if (number <= 0) {
+            // sanity check: don't print anything if the number is 0 or less
+            return;
+        }
+
+        for (int i = 0; i < number; i++) {
+            stars += "*";
+        }
+
+        System.out.println(stars);
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+
+        if (size <= 0) {
+            return;
+        }
+
+        // first line
+        for (int i = 0; i < size; i++) {
+            printStars(size);
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+
+        for (int i = 0; i < size; i++) {
+            printStars(i + 1);
+        }
     }
 }
