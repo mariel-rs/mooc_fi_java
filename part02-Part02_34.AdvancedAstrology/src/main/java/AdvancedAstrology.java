b10501cf73e51b2428676c0ff8abba37e52c3392
mariel-rs <mariel.rsal@gmail.com>
2022-07-31 21:58:11 -0500

    Complete section 4 of part2

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,20 +1,56 @@
 
 public class AdvancedAstrology {
 
-    public static void printStars(int number) {
-        // part 1 of the exercise
-    }
-
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        
+        String spaces = "";
+
+        for (int i = 0; i < number; i++) {
+            spaces += " ";
+        }
+
+        System.out.print(spaces);
+    }
+
+    public static void printStars(int number) {
+
+        String stars = "";
+
+        if (number <= 0) {
+            // sanity check: don't print anything if the number is 0 or less
+            return;
+        }
+
+        for (int i = 0; i < number; i++) {
+            stars += "*";
+        }
+
+        System.out.println(stars);
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        for (int i = 0; i < size; i++) {
+            printSpaces(size - i -1);
+            printStars(i+1);
+        }
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+
+        // "leaves" / triangle part
+        for (int i = 0; i < height; i++) {
+            printSpaces(height - i - 1);
+            printStars(2*i + 1);
+        }
+
+        // base: 2 x 3 rectangle
+        for (int i = 0; i <= 1; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+
     }
 
     public static void main(String[] args) {
@@ -22,7 +58,7 @@ public class AdvancedAstrology {
 
         printTriangle(5);
         System.out.println("---");
-        christmasTree(4);
+        christmasTree(10);
         System.out.println("---");
         christmasTree(10);
     }
