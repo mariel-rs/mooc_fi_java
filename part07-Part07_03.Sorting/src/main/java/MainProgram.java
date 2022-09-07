8a08fd38af1b45405ca30ce3530bbb4a1ea83d51
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-09-06 21:59:23 -0500

    Progress part7 exercises

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,73 @@
+import java.util.Arrays;
 
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
+
         int[] array = {3, 1, 5, 99, 3, 12};
+
+        sort(array);
     }
 
+    public static int smallest(int[] array) {
+
+        int smallest = array[0];
+
+        for (int item : array) {
+            if (item < smallest) {
+                smallest = item;
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        
+        int smallestIdx = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[smallestIdx]) {
+                smallestIdx = i;
+            }
+        }
+        
+        return smallestIdx;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        
+        int smallestIdx = startIndex;
+
+        for (int i = startIndex; i < table.length; i++) {
+            
+            if (table[i] < table[smallestIdx]) {
+                smallestIdx = i;
+            }
+        }
+
+        return smallestIdx;
+    }
+
+    public static void swap(int[]array, int index1, int index2) {
+        
+        int tmp = array[index1];
+
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
+    public static void sort(int[] array) {
+        
+        int i = 0;
+
+        while (i < array.length) {
+            
+            System.out.println(Arrays.toString(array));
+            int smallestIdx = indexOfSmallestFrom(array, i);
+
+            swap(array, i, smallestIdx);
+
+            i += 1;
+        }
+    }
 }
--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,40 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        String[] stringsArray = {"H", "Abc", "Whoop", "Cat", "Bean", "Dress"};
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
+
+        Collections.addAll(numbers, 3, 1, 5, 99, 3, 12);
+        Collections.addAll(strings, "H", "Abc", "Whoop", "Cat", "Bean", "Dress");
+
+        System.out.println(strings.toString());
+        sortStrings(strings);
+        System.out.println(strings.toString());
+
     }
 
+     public static void sort(int[] array) {
+        Arrays.sort(array);
+     }
+
+     public static void sort(String[] array) {
+        Arrays.sort(array);
+     }
+
+     public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+     }
+
+     public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+     }
 }
