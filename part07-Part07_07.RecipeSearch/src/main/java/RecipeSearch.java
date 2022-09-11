9a1b119f54b01a04d1857524429bd2da3f33d73e
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-09-11 00:54:07 -0500

    Complete part7 exercises

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class RecipeSearch {
 
     public static void main(String[] args) {
+
+        RecipeManager manager = new RecipeManager();
         Scanner scanner = new Scanner(System.in);
 
+        UserInterface ui = new UserInterface(manager, scanner);
+        ui.start();
     }
 
 }
