39e5c74985e28df9aa149eb0f61b3b496a3c0a85
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-09-10 21:19:27 -0500

    Progress part7 exercises

--- part07-Part07_08.BigYear/src/main/java/mainProgram.java
+++ part07-Part07_08.BigYear/src/main/java/mainProgram.java
@@ -8,8 +8,11 @@ public class mainProgram {
         // if and when you create other classes, pass the scanner to them
         // as a parameter
 
-        Scanner scan = new Scanner(System.in);
+        BirdManager manager = new BirdManager();
+        Scanner scanner = new Scanner(System.in);
 
+        UserInterface ui = new UserInterface(manager, scanner);
+        ui.start();
     }
 
 }
